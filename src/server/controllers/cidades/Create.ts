cf2520045ef076438f5a81a6b103a0033aa3c93a
frankwillian <frank19valentim@gmail.com>
2024-02-14 22:30:38 -0300

    feat(interface): Implementa interface de cidades
    Este commit adiciona a implementação da interface de cidades ao projeto. A interface inclui componentes e páginas necessárias para visualizar, adicionar, editar e excluir informações sobre cidades. Isso proporciona uma experiência de usuário completa para gerenciamento de dados de cidades.
    
    #21

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -3,19 +3,20 @@ import { StatusCodes } from "http-status-codes";
 import { abort } from "process";
 import * as yup from 'yup';
 import { validation } from "../../shared/middlewares";
+import { ICidade } from "../../database/models";
 
-interface ICitadel {
-  nome: string;
-};
+interface IBodyProps extends ICidade {};
 
 export const createValidation = validation({
     body: yup.object().shape({
     nome: yup.string().required().min(3)
 })});
 
-export const create = async (req: Request<{}, {}, ICitadel>, res: Response) => {
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
 
   console.log(req.body);
+
+  
 
   return res.status(StatusCodes.CREATED).json(1);
 };
