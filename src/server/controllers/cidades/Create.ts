e2666a79eabc6a706850fa00ef87fc6620c94cfe
frankwillian <frank19valentim@gmail.com>
2024-02-15 14:45:05 -0300

    feat(provider): Cria provider para cidade
    Este commit adiciona um provider para cidade ao projeto. O provider encapsula a lógica de negócios relacionada às operações de criação, leitura, atualização e exclusão (CRUD) de cidades. Ele fornece uma interface limpa para interagir com os dados das cidades, seguindo boas práticas de desenvolvimento.
    
    #22

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,7 +1,7 @@
 import { Request, RequestHandler, Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import { abort } from "process";
 import * as yup from 'yup';
+import { CidadesProvider } from "../../database/providers/cidades";
 import { validation } from "../../shared/middlewares";
 import { ICidade } from "../../database/models";
 
@@ -9,14 +9,21 @@ interface IBodyProps extends ICidade {};
 
 export const createValidation = validation({
     body: yup.object().shape({
-    nome: yup.string().required().min(3)
+    nome: yup.string().required().min(3).max(150)
 })});
 
 export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+  const result = await CidadesProvider.create(req.body);
+
+  if (result === null || result === undefined) {
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      errors: {
+        default: 'Erro ao criar cidade'
+      }
+    });
+  }
 
   console.log(req.body);
 
-  
-
-  return res.status(StatusCodes.CREATED).json(1);
+  return res.status(StatusCodes.CREATED).json(result);
 };
