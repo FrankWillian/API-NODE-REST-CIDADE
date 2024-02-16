6c257f3979aa4a449dee07467370731c7b8f0acb
frankwillian <frank19valentim@gmail.com>
2024-02-16 10:37:29 -0300

    feat(configuração): Atualiza dependências para utilizar banco de dados na nuvem
    Este commit atualiza as dependências de produção no arquivo package.json para configurar o projeto para utilizar um banco de dados na nuvem.
    
    #26

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
+import { start } from 'repl';
+import { Knex } from './server/database/knex';
 import server from './server/Server';
 
 server.listen(process.env.PORT || 3333, () => {
   console.log(`App rodando na porta ${process.env.PORT || 3333}`);
 });
+
+if (process.env.IS_LOCALHOST !== 'true') {
+  Knex.migrate.latest()
+  .then(() => {
+    start();
+  })
+.catch(console.log);
+} else {
+  start();
+}
