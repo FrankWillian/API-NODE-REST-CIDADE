29061991ac2eb217ff16484075a8109fcccd1ca5
frankwillian <frank19valentim@gmail.com>
2024-02-16 15:05:29 -0300

    feat(controller): Implementa método create para a entidade Pessoa
    Este commit adiciona a implementação do método create à controller responsável pela entidade Pessoa. Este método permite criar uma nova pessoa no sistema, adicionando seus dados ao banco de dados.
    
    #29.6

--- src/server/database/providers/pessoas/UpdateById.ts
+++ src/server/database/providers/pessoas/UpdateById.ts
@@ -2,14 +2,24 @@ import { ETableNames } from "../../seeds/ETableNames";
 import { IPessoa } from "../../models/Pessoas";
 import { Knex } from "../../knex";
 
-export const UpdateById = async (id: number, cidade: Omit<IPessoa, 'id'>): Promise<void | Error> => {
+export const UpdateById = async (id: number, pessoa: Omit<IPessoa, 'id'>): Promise<void | Error> => {
   try {
+    const [{ count }] = await Knex(ETableNames.pessoa)
+      .where('id', '=', id)
+      .count<[{ count: number}]>('* as count');
+
+    if (count === 0) {
+      return new Error('A cidade usada no cadastro não foi encontrada'); 
+    }
+
     const result = await Knex(ETableNames.pessoa)
+      .update(pessoa)
       .where('id', '=', id)
 
-    //if (result > 0) return ;
+    if (result > 0) return;
 
     return new Error('Erro a atualizar o registo');
+    
   } catch (error) {
     console.log(error);
     return new Error('Erro a atualizar o registo');
