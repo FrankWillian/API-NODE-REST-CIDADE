819547e9781eebebf81b1cf3044cbb770078b1b1
frankwillian <frank19valentim@gmail.com>
2024-02-17 08:24:28 -0300

    feat(controller): Adiciona método getAll à controller de Pessoa
    Este commit adiciona o método getAll à controller responsável pela entidade Pessoa. O método permite recuperar todas as pessoas cadastradas no sistema.
    
    #30.3

--- src/server/controllers/pessoas/GetAll.ts
+++ src/server/controllers/pessoas/GetAll.ts
@@ -1,7 +1,8 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import * as yup from 'yup';
-import { CidadesProvider } from "../../database/providers/cidades";
+
+import { PessoasProvider } from "../../database/providers/pessoas";
 import { validation } from "../../shared/middlewares";
 interface IQueryProps {
   id?: number;
@@ -22,8 +23,8 @@ export const getAllValidation = validation({
 export const getAll = async (req: Request<{}, {}, {}, IQueryProps>, res: Response) => {
   try {
 
-    const result = await CidadesProvider.GetAll(req.query.page || 1, req.query.limit || 7, req.query.filter || '', Number(req.query.id));
-    const count = await CidadesProvider.count(req.query.filter);
+    const result = await PessoasProvider.GetAll(req.query.page || 1, req.query.limit || 7, req.query.filter || '');
+    const count = await PessoasProvider.count(req.query.filter);
 
     if (result instanceof Error) {
       return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ errors: { default: result.message } });
