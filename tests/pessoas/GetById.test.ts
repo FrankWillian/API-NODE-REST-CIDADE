1767f2f130407d64551f7180d60e90d910bc1e94
frankwillian <frank19valentim@gmail.com>
2024-02-17 09:30:56 -0300

    test: Adiciona teste de getById para a entidade Pessoa
    Este commit adiciona o teste de getById para a entidade Pessoa. O teste verifica se a rota de getById retorna corretamente os detalhes de uma pessoa específica com base no ID fornecido. Foram criados casos de teste para diferentes cenários, incluindo quando a pessoa existe e quando o ID fornecido não corresponde a nenhuma pessoa no sistema.
    
    #30.3

--- tests/pessoas/GetById.test.ts
+++ tests/pessoas/GetById.test.ts
@@ -3,32 +3,39 @@ import { testServer } from "../jest.setup";
 import { expect, test, beforeAll, afterAll } from '@jest/globals';
 
 describe('Cidades - GetById,', () => {
+
+  let cidadeId: number | undefined = undefined;
+  beforeAll(async () => {
+
+  })
   
   it('Busca registro', async () => {
-
     const res1 = await testServer
-     .post('/cidades')
-     .send({ nome: 'Caxias do sul'});
+     .post('/pessoas')
+     .send({ 
+      cidadeId,
+      nomeCompleto: 'Frank Willian',
+      email: 'frankwillian@gmail.com.br'
+    });
 
      expect(res1.statusCode).toEqual(StatusCodes.CREATED);
 
      const resBuscada = await testServer
-      .get(`/cidades/${res1.body}`)
+      .get(`/pessoas/${res1.body}`)
       .send();
 
     expect(resBuscada.statusCode).toEqual(StatusCodes.OK);
-    expect(resBuscada.body).toHaveProperty('nome');
+    expect(resBuscada.body).toHaveProperty('nomeCompleto');
 
   });
-
 
   it('Tenta buscar registro que não existe', async () => {
 
     const res1 = await testServer
-    .delete('/cidades/999999')
+    .delete('/pessoas/999999')
     .send();
 
-    expect(res1.statusCode).toEqual(StatusCodes.NOT_FOUND);
+    expect(res1.statusCode).toEqual(StatusCodes.INTERNAL_SERVER_ERROR);
     expect(res1.body).toHaveProperty('error.default');
   });
 });
