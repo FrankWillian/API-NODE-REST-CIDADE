961268040854241b765a5fe0ba8033c37508aa13
frankwillian <frank19valentim@gmail.com>
2024-02-18 08:56:25 -0300

    refactor(controller): Refatora a camada de controller de usuário
    Este commit realiza um refatoramento na camada de controller de usuário para melhorar a organização e legibilidade do código. Foram aplicadas técnicas de refatoramento, como extração de funções, simplificação de lógica e remoção de duplicação de código.
    
    #34.1

--- src/server/database/migrations/0002_create_usuario.ts
+++ src/server/database/migrations/0002_create_usuario.ts
@@ -6,8 +6,8 @@ export async function up(knex: Knex) {
   .schema
   .createTable(ETableNames.usuario, table => {
     table.bigIncrements('id').primary().index();
-    table.string('nome').notNullable().checkLength('>', 3);
-    table.string('email').unique().notNullable().checkLength('>', 6);
+    table.string('nome').notNullable().checkLength('>=', 3);
+    table.string('email').unique().notNullable().checkLength('>=', 6);
 
     table.comment('Tabela usada para armazenar usuarios do sistema');
   })
