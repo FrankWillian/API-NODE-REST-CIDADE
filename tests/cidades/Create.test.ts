82fffbc9b750ac4e5b62753c7e018dfa65d8f1dc
frankwillian <frank19valentim@gmail.com>
2024-03-09 22:03:07 -0300

    test(refactor): Refatora testes para incluir autenticação via JWT
    Este commit atualiza os testes existentes para acomodar a nova camada de autenticação via JSON Web Tokens (JWT). Foram adicionados mocks e stubs para simular a autenticação de usuários, permitindo que os testes verifiquem tanto os fluxos de sucesso quanto os de falha relacionados à autenticação. Isso garante que a integridade e a segurança da aplicação sejam mantidas em todas as funcionalidades testadas.
    
    #39

--- tests/cidades/Create.test.ts
+++ tests/cidades/Create.test.ts
@@ -2,9 +2,21 @@ import { StatusCodes } from 'http-status-codes';
 import { testServer } from '../jest.setup';
 
 describe('Cidades - Create', () => {
+
+  let acessToken = '';
+
+  beforeAll(async () => {
+    const email = 'create-cidade@gmail.com';
+    await testServer.post('/cadastrar').send({ nome: 'Teste', email, senha: '123456'});
+    const signInRes = await testServer.post('/entrar').send({ email, senha: '123456'});
+
+    acessToken = signInRes.body.acessToken;
+  });
+
   it('Cria registro', async () =>{
     const res1 = await testServer 
       .post('/cidades')
+      .set({Authorization: `Bearer ${acessToken}`})
       .send({ nome: 'Caxias do Sul'});
     expect(res1.statusCode).toEqual(StatusCodes.CREATED);
     expect(typeof res1.body).toEqual('number');
@@ -12,6 +24,7 @@ describe('Cidades - Create', () => {
   it('Tenta criar registro com nome muito curto', async () =>{
     const res1 = await testServer 
       .post('/cidades')
+      .set({Authorization: `Bearer ${acessToken}`})
       .send({ nome: 'Ca'});
     expect(res1.statusCode).toEqual(StatusCodes.BAD_REQUEST);
     expect(res1.body).toHaveProperty('error.body.nome');
